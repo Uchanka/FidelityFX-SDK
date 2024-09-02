58e6d10405ef08ab3e8d28d3f86b67d5617d13fc
AIPC <grillnov@gmail.com>
2024-09-02 05:06:42 -0700

    [SDK] How to link samples with SDK?

--- sdk/src/components/vpufsr/ffx_frameinterpolation_vpumod.cpp
+++ sdk/src/components/vpufsr/ffx_frameinterpolation_vpumod.cpp
@@ -1,25 +1,3 @@
-// This file is part of the FidelityFX SDK.
-//
-// Copyright (C) 2024 Advanced Micro Devices, Inc.
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #include <algorithm>    // for max used inside SPD CPU code.
 #include <cmath>        // for fabs, abs, sinf, sqrt, etc.
 #include <string>       // for memset
@@ -32,7 +10,7 @@
 #include <FidelityFX/gpu/spd/ffx_spd.h>
 #include <ffx_object_management.h>
 
-#include "ffx_frameinterpolation_private.h"
+#include "ffx_frameinterpolation_vpumod.h"
 
 // lists to map shader resource bindpoint name to resource identifier
 typedef struct ResourceBinding
